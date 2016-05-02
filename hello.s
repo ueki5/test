e7d60e025048c13fca1fb1d025ed5a46f64c981f
ueki5 <ueki005@gmail.com>
2016-05-02 13:22:58 +0900

    Added files via upload

---
+++ hello.s
@@ -0,0 +1,23 @@
+	.file	"hello.c"
+	.section	.rodata
+.LC0:
+	.string	"Hello World! %d %s\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	pushl	%ebp
+	movl	%esp, %ebp
+	andl	$-16, %esp
+	subl	$16, %esp
+	movl	12(%ebp), %eax
+	movl	(%eax), %eax
+	movl	%eax, 8(%esp)
+	movl	8(%ebp), %eax
+	movl	%eax, 4(%esp)
+	movl	$.LC0, (%esp)
+	call	printf
+	movl	$0, %eax
+	leave
+	ret
